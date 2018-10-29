c66c593bce7042c89edb3b70dd4640072bcfe0d5
Brandon Black <d2sm10@hotmail.com>
2018-10-29 08:34:35 -0600

    Modified several base templates.

--- lib/block_stack/server/template/templates/general.rb
+++ lib/block_stack/server/template/templates/general.rb
@@ -1,21 +1,23 @@
 module BlockStack
 
-  add_template(:welcome, :block_stack, :get_api, '/', type: :route) do
+  add_template(:welcome, :block_stack_api, :get_api, '/', type: :route) do
     {
-      message: 'Welcome to BlockStack!',
+      message: params[:message] || "Welcome to #{self.class.to_s}",
       application: self.class.to_s,
-      time: Time.now,
-      verion: BlockStack::VERSION,
-      ruby: RUBY_VERSION,
-      os: BBLib::OS.os
+      system: {
+        block_stack_version: BlockStack::VERSION,
+        ruby: RUBY_VERSION,
+        time: Time.now.to_f,
+        os: BBLib::OS.os
+      }
     }
   end
 
-  add_template(:routes, :block_stack, :get_api, '/routes', type: :route) do
+  add_template(:routes, :block_stack_api, :get_api, '/routes', type: :route) do
     self.class.route_map
   end
 
-  add_template(:time, :block_stack, :get_api, '/time', type: :route) do
+  add_template(:time, :block_stack_api, :get_api, '/time', type: :route) do
     { time: params[:time_format] ? Time.now.strftime(params[:time_format]) : Time.now.to_f }
   end
 end
